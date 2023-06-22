8702ece89c5dd841cbb7ca5dcc0a0dd5738e43c8
eopong87 <eopong87@gmail.com>
2023-06-22 08:44:47 -0400

    exercise 1

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Exercise 1
+
+            Console.WriteLine("Guess That Number Game ");
+            Console.WriteLine("See if you can get that number?");
+            
+            //var userResponse = Console.ReadLine();
+
+            //var upperLimit = int.Parse(userResponse);
+            var random = new Random();
+            var number = random.Next (1, 5);
+            Console.WriteLine("Input your Guess:");
+
+            var guess = int.Parse(Console.ReadLine());
+                
+
+           if(guess > number)
+            {
+                Console.WriteLine("sorry too high! Try Again");
+            }
+
+            else if(guess < number)
+            {
+                Console.WriteLine("Your guess is too low! Sorry");
+            }
+
+            else
+            {
+                Console.WriteLine("Correct");
+            }
+     
+
+           //start exercise 2 here:
+
+
+
+
+           
         }
+
     }
 }
